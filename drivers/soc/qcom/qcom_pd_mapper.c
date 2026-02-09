d7862b2f4133680b3f7307d7e076b7b35a7e5155
Mohammad Rafi Shaik <mohammad.rafi.shaik@oss.qualcomm.com>
2026-02-09 16:59:47 +0530

    FROMLIST: soc: qcom: pd-mapper: Add support for QCS8300
    Add support for the Qualcomm QCS8300 SoC to the protection
    domain mapper. QCS8300 share the same protection domain
    configuration as SC8280XP, except charger_pd.
    
    Add an entry to the kernel, to avoid the need for userspace to
    provide this service.
    
    Link: https://lore.kernel.org/all/20260209112947.930853-3-mohammad.rafi.shaik@oss.qualcomm.com/
    Signed-off-by: Mohammad Rafi Shaik <mohammad.rafi.shaik@oss.qualcomm.com>

--- drivers/soc/qcom/qcom_pd_mapper.c
+++ drivers/soc/qcom/qcom_pd_mapper.c
@@ -431,6 +431,13 @@ static const struct qcom_pdm_domain_data *qcs615_domains[] = {
 	NULL,
 };
 
+static const struct qcom_pdm_domain_data *qcs8300_domains[] = {
+	&adsp_audio_pd,
+	&adsp_root_pd,
+	&cdsp_root_pd,
+	NULL,
+};
+
 static const struct qcom_pdm_domain_data *sa8775p_domains[] = {
 	&adsp_audio_pd,
 	&adsp_root_pd,
@@ -616,6 +623,7 @@ static const struct of_device_id qcom_pdm_domains[] __maybe_unused = {
 	{ .compatible = "qcom,qcm6490", .data = sc7280_domains, },
 	{ .compatible = "qcom,qcs404", .data = qcs404_domains, },
 	{ .compatible = "qcom,qcs615", .data = qcs615_domains, },
+	{ .compatible = "qcom,qcs8300", .data = qcs8300_domains, },
 	{ .compatible = "qcom,sa8775p", .data = sa8775p_domains, },
 	{ .compatible = "qcom,sc7180", .data = sc7180_domains, },
 	{ .compatible = "qcom,sc7280", .data = sc7280_domains, },
